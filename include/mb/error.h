31ea9fefc2dc0c909b1039f1240659e1a435b4b4
Bednarek Mikołaj <mikolaj@bednarek.xyz>
2022-08-20 09:31:34 +0200

    added a virtual destructor for the basic_error

--- include/mb/error.h
+++ include/mb/error.h
@@ -8,16 +8,17 @@ class basic_error {
 public:
     using ptr = std::unique_ptr<basic_error>;
 
-private:
-    std::string m_message;
-
-public:
     explicit basic_error(std::string message) noexcept;
+    virtual ~basic_error() = default;
+
     bool operator==(const basic_error &other) const;
 
     [[nodiscard]] virtual const std::string &msg() const;
 
-    ptr copy() const;
+    [[nodiscard]] ptr copy() const;
+
+private:
+    std::string m_message;
 };
 
 basic_error::ptr error(std::string message);
